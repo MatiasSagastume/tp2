b0d9e2091fedb291c67fd3f05ec71987efc1b80f
Matias <128936769+MatiasSagastume@users.noreply.github.com>
2023-11-18 03:09:42 -0300

    Update errores.go

--- tp2/codigo/errores/errores.go
+++ tp2/codigo/errores/errores.go
@@ -1,5 +1,11 @@
 package errores
 
+type ErrorLeerArchivo struct{}
+
+func (e ErrorLeerArchivo) Error() string {
+	return "ERROR: Lectura de archivos"
+}
+
 type ErrorParametros struct{}
 
 func (e ErrorParametros) Error() string {
@@ -9,35 +15,35 @@ func (e ErrorParametros) Error() string {
 type ErrorUsuarioLogueado struct{}
 
 func (e ErrorUsuarioLogueado) Error() string {
-	return "Error: Ya habia un usuario loggeado."
+	return "Error: Ya habia un usuario loggeado"
 }
 
 type ErrorUsuarioNoExiste struct{}
 
 func (e ErrorUsuarioNoExiste) Error() string {
-	return "Error: usuario no existente."
+	return "Error: usuario no existente"
 }
 
 type ErrorNadieLoggeado struct{}
 
 func (e ErrorNadieLoggeado) Error() string {
-	return "Error: no habia usuario loggeado."
+	return "Error: no habia usuario loggeado"
 }
 
-type ErrorNoHayPosts struct{}
+type ErrorNoHayPostsOLogueado struct{}
 
-func (e ErrorNoHayPosts) Error() string {
+func (e ErrorNoHayPostsOLogueado) Error() string {
 	return "Usuario no loggeado o no hay mas posts para ver"
 }
 
 type ErrorPostLikear struct{}
 
 func (e ErrorPostLikear) Error() string {
-	return "Error: Usuario no loggeado o Post inexistente."
+	return "Error: Usuario no loggeado o Post inexistente"
 }
 
 type ErrorPostLikeados struct{}
 
 func (e ErrorPostLikeados) Error() string {
-	return "Error: Post inexistente o sin likes."
+	return "Error: Post inexistente o sin likes"
 }
