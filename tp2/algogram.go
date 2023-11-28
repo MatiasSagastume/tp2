001a9db86ff899599ba1a69411976ac6d4975e64
Matias <128936769+MatiasSagastume@users.noreply.github.com>
2023-11-28 18:41:29 -0300

    Correcciones

--- tp2/algogram.go
+++ tp2/algogram.go
@@ -14,6 +14,19 @@ const (
 	UBICACION_ARCH_USUARIOS = 0
 	LARGO_PARAMETROS        = 1
 	ESPACIO                 = " "
+	UBICACION_COMANDO       = 0
+	INDICE_PARAMETROS       = 1
+	LOGIN                   = "login"
+	LOGOUT                  = "logout"
+	SALUDO                  = "Hola"
+	MENSAJE_DESPEDIDA       = "Adios"
+	PUBLICAR                = "publicar"
+	MENSAJE_PUBLICAR_POST   = "Post publicado"
+	SIGUIENTE_FEED          = "ver_siguiente_feed"
+	LIKEAR                  = "likear_post"
+	MENSAJE_POST_LIKEADO    = "Post likeado"
+	MOSTRAR_LIKES           = "mostrar_likes"
+	OPCION_INVALIDA         = ": la opcion ingresada no es valida"
 )
 
 func main() {
@@ -23,24 +36,24 @@ func main() {
 		return
 	}
 	rutaUsuarios := parametros[UBICACION_ARCH_USUARIOS]
-	diccUsuarios, err := crearDiccionarioUsuarios(rutaUsuarios)
+	baseDeDatosUsuarios, err := crearBaseDeDatosUsuarios(rutaUsuarios)
 	if err != nil {
 		fmt.Println(errores.ErrorLeerArchivo{})
 		return
 	}
-	listaDePosts := make([]algogram_tdas.Post, 0, 1)
-	lectura(diccUsuarios, listaDePosts)
+	arrayDePosts := make([]algogram_tdas.Post, 0, 1)
+	lecturaInput(baseDeDatosUsuarios, arrayDePosts)
 
 }
 
-func crearDiccionarioUsuarios(ruta string) (algogram_tdas.DiccionarioUsuarios, error) {
-	res := algogram_tdas.CrearDiccionarioDeUsuarios()
+func crearBaseDeDatosUsuarios(ruta string) (algogram_tdas.BaseDeDatosUsuarios, error) {
+	res := algogram_tdas.CrearBaseDeDatosUsuarios()
 	errorGenerico := errores.ErrorLeerArchivo{}
-	archLista, err := os.Open(ruta)
+	archUsuarios, err := os.Open(ruta)
 	if err != nil {
 		return res, errorGenerico
 	}
-	lector := bufio.NewScanner(archLista)
+	lector := bufio.NewScanner(archUsuarios)
 	for lector.Scan() {
 		nombreUsuario := lector.Text()
 		res.AgregarUsuario(algogram_tdas.CrearUsuario(nombreUsuario, res.Cantidad()))
@@ -48,45 +61,45 @@ func crearDiccionarioUsuarios(ruta string) (algogram_tdas.DiccionarioUsuarios, e
 	return res, nil
 }
 
-func lectura(diccUsuarios algogram_tdas.DiccionarioUsuarios, listaDePosts []algogram_tdas.Post) {
+func lecturaInput(baseDeDatosUsuarios algogram_tdas.BaseDeDatosUsuarios, arrayDePosts []algogram_tdas.Post) {
 	escaner := bufio.NewScanner(os.Stdin)
 	var usuarioLogueado algogram_tdas.Usuario
 	for escaner.Scan() {
 		entrada := escaner.Text()
 		palabras := strings.Split(entrada, ESPACIO)
-		comando := palabras[0]
-		parametroIngresado := strings.Join(palabras[1:], ESPACIO)
+		comando := palabras[UBICACION_COMANDO]
+		parametroIngresado := strings.Join(palabras[INDICE_PARAMETROS:], ESPACIO)
 		switch {
-		case comando == "login":
+		case comando == LOGIN:
 			if hayAlguienLogueado(usuarioLogueado) {
 				fmt.Println(errores.ErrorUsuarioLogueado{}.Error())
 				continue
 			}
-			usuario, err := login(parametroIngresado, diccUsuarios)
+			usuario, err := login(parametroIngresado, baseDeDatosUsuarios)
 			if err != nil {
 				fmt.Println(err.Error())
 				continue
 			}
 			usuarioLogueado = usuario
-			fmt.Println("Hola", usuario.LeerNombreDeUsuario())
+			fmt.Println(SALUDO, usuario.LeerNombreDeUsuario())
 
-		case comando == "logout":
+		case comando == LOGOUT:
 			if !hayAlguienLogueado(usuarioLogueado) {
 				fmt.Println(errores.ErrorNadieLoggeado{}.Error())
 				continue
 			}
 			usuarioLogueado = nil
-			fmt.Println("Adios")
+			fmt.Println(MENSAJE_DESPEDIDA)
 
-		case comando == "publicar":
+		case comando == PUBLICAR:
 			if !hayAlguienLogueado(usuarioLogueado) {
 				fmt.Println(errores.ErrorNadieLoggeado{}.Error())
 				continue
 			}
-			publicarPost(usuarioLogueado, parametroIngresado, &listaDePosts, diccUsuarios)
-			fmt.Println("Post publicado")
+			publicarPost(usuarioLogueado, parametroIngresado, &arrayDePosts, baseDeDatosUsuarios)
+			fmt.Println(MENSAJE_PUBLICAR_POST)
 
-		case comando == "ver_siguiente_feed":
+		case comando == SIGUIENTE_FEED:
 			if !hayAlguienLogueado(usuarioLogueado) {
 				fmt.Println(errores.ErrorNoHayPostsOLogueado{}.Error())
 				continue
@@ -98,30 +111,30 @@ func lectura(diccUsuarios algogram_tdas.DiccionarioUsuarios, listaDePosts []algo
 			}
 			fmt.Println(post.MostrarPost())
 
-		case comando == "likear_post":
+		case comando == LIKEAR:
 			if !hayAlguienLogueado(usuarioLogueado) {
 				fmt.Println(errores.ErrorPostLikear{}.Error())
 				continue
 			}
-			err := likearPost(usuarioLogueado, parametroIngresado, listaDePosts)
+			err := likearPost(usuarioLogueado, parametroIngresado, arrayDePosts)
 			if err != nil {
 				fmt.Println(err.Error())
 				continue
 			}
-			fmt.Println("Post likeado")
+			fmt.Println(MENSAJE_POST_LIKEADO)
 
-		case comando == "mostrar_likes":
-			fmt.Println(mostrarLikes(parametroIngresado, listaDePosts))
+		case comando == MOSTRAR_LIKES:
+			fmt.Println(mostrarLikes(parametroIngresado, arrayDePosts))
 
 		default:
-			fmt.Println(comando, ": la opcion ingresada no es valida")
+			fmt.Println(comando, OPCION_INVALIDA)
 			continue
 		}
 	}
 }
 
-func login(nombre string, diccUsuarios algogram_tdas.DiccionarioUsuarios) (algogram_tdas.Usuario, error) {
-	usuario, err := diccUsuarios.DevolverUsuario(nombre)
+func login(nombre string, BaseUsuarios algogram_tdas.BaseDeDatosUsuarios) (algogram_tdas.Usuario, error) {
+	usuario, err := BaseUsuarios.DevolverUsuario(nombre)
 	return usuario, err
 }
 
@@ -129,25 +142,25 @@ func hayAlguienLogueado(usuarioLogueado algogram_tdas.Usuario) bool {
 	return usuarioLogueado != nil
 }
 
-func publicarPost(usuario algogram_tdas.Usuario, texto string, listaDePosts *[]algogram_tdas.Post, diccUsuarios algogram_tdas.DiccionarioUsuarios) {
-	post := usuario.PublicarPost(len(*listaDePosts), texto)
-	*listaDePosts = append(*listaDePosts, post)
-	diccUsuarios.AgregarPost(post)
+func publicarPost(usuario algogram_tdas.Usuario, texto string, arrayDePosts *[]algogram_tdas.Post, BaseUsuarios algogram_tdas.BaseDeDatosUsuarios) {
+	post := usuario.PublicarPost(len(*arrayDePosts), texto)
+	*arrayDePosts = append(*arrayDePosts, post)
+	BaseUsuarios.AgregarPost(post)
 }
 
-func likearPost(usuario algogram_tdas.Usuario, id string, listaDePosts []algogram_tdas.Post) error {
+func likearPost(usuario algogram_tdas.Usuario, id string, arrayDePosts []algogram_tdas.Post) error {
 	nroId, err := strconv.Atoi(id)
-	if err != nil || nroId >= len(listaDePosts) {
+	if err != nil || nroId >= len(arrayDePosts) {
 		return errores.ErrorPostLikear{}
 	}
-	listaDePosts[nroId].RecibirLike(usuario)
+	arrayDePosts[nroId].RecibirLike(usuario)
 	return nil
 }
 
-func mostrarLikes(id string, listaDePosts []algogram_tdas.Post) string {
+func mostrarLikes(id string, arrayDePosts []algogram_tdas.Post) string {
 	nroId, err := strconv.Atoi(id)
-	if err != nil || nroId >= len(listaDePosts) {
+	if err != nil || nroId >= len(arrayDePosts) {
 		return errores.ErrorPostLikeados{}.Error()
 	}
-	return listaDePosts[nroId].MostrarLikes()
+	return arrayDePosts[nroId].MostrarLikes()
 }
